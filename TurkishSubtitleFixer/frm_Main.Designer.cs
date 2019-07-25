c1d15ab437511e1ce4cebe06e61f81f07e39ce74
Hosseinpourziyaie <hosseinpourziyaie@gmail.com>
2019-07-25 19:13:43 +0430

    Add files via upload

---
+++ TurkishSubtitleFixer/frm_Main.Designer.cs
@@ -0,0 +1,115 @@
+﻿using System.Windows.Forms;
+using System.Drawing;
+
+namespace TurkishSubtitleFixer
+{
+    partial class frm_Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frm_Main));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(271, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "This application fixes subtitle unicode forTurkish subtitle \r\nfiles (.srt) causes" +
+    " Turkish language independent \r\ncharacter-set shown correctly in video players";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(55, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(266, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Drag and Drop your SRT file(s) here!";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(96, 87);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(182, 13);
+            this.linkLabel1.TabIndex = 2;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://twitter.com/hosseinpourziyaie";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(44, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "This Program is Developed by Hosseinpourziyaie \r\nMain Source Based on Shaahin Per" +
+    "sian Subtitle Fixer Code";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // frm_Main
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(378, 138);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frm_Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Turkish Subtitle Fixer";
+            this.Load += new System.EventHandler(this.frm_Main_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.frm_Main_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.frm_Main_DragEnter);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private LinkLabel linkLabel1;
+    }
+}
+
---
+++ TurkishSubtitleFixer/frm_Main.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+
+namespace TurkishSubtitleFixer
+{
+    public partial class frm_Main : Form
+    {
+        public frm_Main(string[] args)
+        {
+            InitializeComponent();
+            fix_subtitles(args);
+        }
+
+        private void frm_Main_Load(object sender, EventArgs e)
+        {
+            //This application fixes subtitle unicode for Turkish subtitle 
+            //files (.srt) that it will be shown correctly on every DVD player
+        }
+
+        private void frm_Main_DragEnter(object sender, DragEventArgs e)
+        {
+            //if (e.Data.GetDataPresent(DataFormats.FileDrop)) 
+            //e.Effect = DragDropEffects.Copy;
+
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false))
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        private void frm_Main_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] array = (string[])e.Data.GetData(DataFormats.FileDrop);
+            fix_subtitles(array);
+        }
+
+        private void fix_subtitles(string[] files)
+        {
+            for (int i = 0; i < files.Length; i++)
+            {
+                if (Path.GetExtension(files[i]) == ".srt")
+                {
+                    try
+                    {
+                        string text = files[i];
+                        StreamReader streamReader = new StreamReader(text, Encoding.GetEncoding("ISO-8859-9"));
+                        string value = streamReader.ReadToEnd();
+                        streamReader.Close();
+                        System.IO.File.Move(text, text + ".bkup");
+                        StreamWriter streamWriter = new StreamWriter(text, false, Encoding.UTF8);
+                        streamWriter.Write(value);
+                        streamWriter.Close();
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Bad or corrupted file!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Unknown file type!");
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://twitter.com/hosseinpourziyaie");
+        }
+    }
+}
